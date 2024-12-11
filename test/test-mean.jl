9aaa757a37edee86b1236f0cf8ca2d06dff46e10
= <j.p.grainger2@outlook.com>
2024-12-11 16:30:03 +0100

    update mean tests

--- test/test-mean.jl
+++ test/test-mean.jl
@@ -6,7 +6,7 @@ grid = CartesianGrid((0.0, 0.0), (3.0, 3.0), dims = (3, 3))
 pointdata = georef((marks = marks,), points)
 griddata = georef((rf = rf,), grid)
 
-@test SpatialMultitaper.mean_estimate(pointdata, region, DefaultMean()) == 3 / 2 # (1+2+3)/4 (because of the region, and that the region has measure 4)
-@test SpatialMultitaper.mean_estimate(griddata, region, DefaultMean()) == 3.0 # (1+2+4+5)/4 (because of the region)
-@test SpatialMultitaper.mean_estimate(pointdata, points, region, KnownMean(1.0)) == 1.0
-@test SpatialMultitaper.mean_estimate(griddata, grid, region, KnownMean(1.0)) == 1.0
+@test Spmt.mean_estimate(pointdata, region, DefaultMean()) == 3 / 2 # (1+2+3)/4 (because of the region, and that the region has measure 4)
+@test Spmt.mean_estimate(griddata, region, DefaultMean()) == 3.0 # (1+2+4+5)/4 (because of the region)
+@test Spmt.mean_estimate(pointdata, region, KnownMean(1.0)) == 1.0
+@test Spmt.mean_estimate(griddata, region, KnownMean(1.0)) == 1.0

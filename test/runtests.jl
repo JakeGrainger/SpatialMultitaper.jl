fcdb985977a59a738587394663d8ab1aeb1c4c3c
JakeGrainger <j.p.grainger2@outlook.com>
2024-12-12 13:47:21 +0100

    make tests run tests in subfolders

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,9 @@
 using SpatialMultitaper
 import SpatialMultitaper as Spmt
 using Test
+include("SpatialMultitaperTestingUtils.jl")
+using .SpatialMultitaperTestingUtils
 
-function slow_dft(u, f, freq, iflag)
-	pm = iflag ≥ 0 ? 1 : -1
-	return [
-		sum(f[i] * exp(pm * 2pi * 1im * sum(u[i] .* k)) for i in eachindex(u, f)) for
-		k in freq
-	]
-end
 #=
 Don't add your tests to runtests.jl. Instead, create files named
 
@@ -16,21 +11,21 @@ Don't add your tests to runtests.jl. Instead, create files named
 
 The file will be automatically included inside a `@testset` with title "Title For My Test".
 =#
-function get_tests(root)
-    for (root, dirs, files) in walkdir(root)
+function get_tests(folder)
+    for (root, dirs, files) in walkdir(folder)
+        for dir in dirs
+            title = titlecase(replace(dir, "-" => " "))
+            @testset "$title" begin
+                get_tests(joinpath(root, dir))
+            end
+        end
         for file in files
             if isnothing(match(r"^test-.*\.jl$", file))
                 continue
             end
             title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
             @testset "$title" begin
-                include(file)
-            end
-        end
-        for dir in dirs
-            title = titlecase(replace(dir, "-" => " "))
-            @testset begin title
-                get_tests(joinpath(root, dir))
+                include(joinpath(root, file))
             end
         end
     end

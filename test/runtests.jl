c9fcc3ce76ccf77e7e3234f30761bc7877e6d421
JakeGrainger <j.p.grainger2@outlook.com>
2024-12-12 15:35:42 +0100

    fix run tests to only include once

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,23 +11,14 @@ Don't add your tests to runtests.jl. Instead, create files named
 
 The file will be automatically included inside a `@testset` with title "Title For My Test".
 =#
-function get_tests(folder)
-    for (root, dirs, files) in walkdir(folder)
-        for dir in dirs
-            title = titlecase(replace(dir, "-" => " "))
-            @testset "$title" begin
-                get_tests(joinpath(root, dir))
-            end
+for (root, dirs, files) in walkdir(@__DIR__)
+    for file in files
+        if isnothing(match(r"^test-.*\.jl$", file))
+            continue
         end
-        for file in files
-            if isnothing(match(r"^test-.*\.jl$", file))
-                continue
-            end
-            title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
-            @testset "$title" begin
-                include(joinpath(root, file))
-            end
+        title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
+        @testset "$title" begin
+            include(joinpath(root, file))
         end
     end
 end
-get_tests(@__DIR__)

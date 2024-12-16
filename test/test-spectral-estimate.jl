3449d5c758ec81d4770ddadc94a9b909a15985ef
JakeGrainger <j.p.grainger2@outlook.com>
2024-12-16 10:50:26 +0100

    use sin tapers for tests to remove unneeded warnings

--- test/test-spectral-estimate.jl
+++ test/test-spectral-estimate.jl
@@ -2,10 +2,7 @@
     region = Box(Point(0), Point(3))
     pattern = georef((a = ones(4),), PointSet([Point(0), Point(1), Point(0.5), Point(0.3)]))
     griddata = georef((rf = rand(6),), CartesianGrid((0,), (3,), dims = (6,)))
-    tapers = interpolated_taper_family(
-        [ones(3), ones(3), ones(3)],
-        CartesianGrid((0,), (3,), dims = (3,)),
-    )
+    tapers = sin_taper_family((3,), region)
     nfreq = (10,)
     fmax = (2,)
     mt_est = multitaper_estimate(
@@ -34,10 +31,7 @@ end
     region = Box(Point(0, 0), Point(3, 3))
     pattern = georef((marks = [1, 2.4],), PointSet([Point(0, 0), Point(1, 1)]))
     griddata = georef((rf = rand(6 * 6),), CartesianGrid((0, 0), (3, 3), dims = (6, 6)))
-    tapers = interpolated_taper_family(
-        [ones(3, 3), ones(3, 3), ones(3, 3)],
-        CartesianGrid((0, 0), (3, 3), dims = (3, 3)),
-    )
+    tapers = sin_taper_family((3, 1), region)
     nfreq = (10, 10)
     fmax = (2, 2)
     mt_est = multitaper_estimate(
@@ -85,10 +79,7 @@ end
         georef((rf = fill(NaN, 6 * 6),), CartesianGrid((0, 0), (3, 3), dims = (6, 6)))
 
     region = Box(Point(0, 0), Point(3, 3))
-    tapers = interpolated_taper_family(
-        [ones(3, 3), ones(3, 3), ones(3, 3)],
-        CartesianGrid((0, 0), (3, 3), dims = (3, 3)),
-    )
+    tapers = sin_taper_family((3, 1), region)
     nfreq = (10, 10)
     fmax = (2, 2)
 

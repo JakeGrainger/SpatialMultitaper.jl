c81aba49130831e73b7cd339df3407ace7535cbe
JakeGrainger <j.p.grainger2@outlook.com>
2025-07-09 16:40:33 +0200

    correct typing for transforms

--- src/transforms/spectral_matrix_transforms.jl
+++ src/transforms/spectral_matrix_transforms.jl
@@ -1,7 +1,10 @@
-function apply_transform(transform::F, power::AbstractArray{SMatrix{P, P, T, L}, D}) where {F, P, T, L, D}
+function apply_transform(
+    transform::F,
+    power::AbstractArray{SMatrix{P,P,T,L},D},
+) where {F,P,T,L,D}
     return transform.(power)
 end
 
-function apply_transform(transform, power::AbstractArray{Number, D}) where {D}
+function apply_transform(transform, power::AbstractArray{<:Number,D}) where {D}
     mapslices(transform, power, dims = (1, 2))
 end

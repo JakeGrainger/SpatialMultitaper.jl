d9f4f232c6c11f4ab16ee7d8bb230d514ca2f876
JakeGrainger <j.p.grainger2@outlook.com>
2025-09-11 18:10:00 +0200

    geo ref should take tuple

--- src/resampling/partial_null_generation.jl
+++ src/resampling/partial_null_generation.jl
@@ -96,7 +96,7 @@ function create_single_intensity(idx, intensities, kernels_ft, J_n::NTuple{P}) w
         (length(freq_version) * prod(step.(freq))) .*
         fftshift(ifft(ifftshift(freq_version)))
     grid_sides = fftshift.(fftfreq.(length.(freq), length.(freq) ./ step.(freq)))
-    return georef(intensity_partial, side2grid(grid_sides))
+    return georef((intensity = vec(intensity_partial),), side2grid(grid_sides))
 end
 
 # commented out is for anisotropic version

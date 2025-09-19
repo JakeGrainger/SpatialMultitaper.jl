e33e2a62f2590b3d33ae713c3f59a608fadf32f5
JakeGrainger <j.p.grainger2@outlook.com>
2025-09-19 12:18:00 +0200

    should Ref args that are passed to apply_transform in case they are arrays

--- src/transforms/spectral_matrix_transforms.jl
+++ src/transforms/spectral_matrix_transforms.jl
@@ -1,11 +1,11 @@
 function apply_transform(
-    transform::F,
-    power::AbstractArray{SMatrix{P,P,T,L},D},
-    args...,
-) where {F,P,T,L,D}
-    return transform.(power, args...)
+        transform::F,
+        power::AbstractArray{SMatrix{P, P, T, L}, D},
+        args...
+) where {F, P, T, L, D}
+    return transform.(power, Ref.(args)...)
 end
 
-function apply_transform(transform, power::AbstractArray{<:Number,D}, args...) where {D}
+function apply_transform(transform, power::AbstractArray{<:Number, D}, args...) where {D}
     mapslices(transform, power, dims = (1, 2), args...)
 end
